d00160c7fdb081758456d838154f378f7394f6d9
Sterling Forest <sterling.kxz@gmail.com>
2013-10-05 20:10:46 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clairvoyance::Application.config.secret_token = 'ddbc795f1fcfd814d0bb0f2ec4c761546d53c14048b7748892eec31da63ee7197e0a67ac1e33429674ed132a0d7b7bef5802b761ee7febf812aa991a9f29679f'
