8b35d8e3d6514a67c666ecffe8251780381d844e
Sterling Forest <sterling.kxz@gmail.com>
2013-10-10 19:47:56 -0400

    adds card description form and moves card css into its own folder

--- app/assets/javascripts/card_view_hidden.js
+++ app/assets/javascripts/card_view_hidden.js
@@ -38,30 +38,43 @@
 // </script>
 
 $(document).ready(function () {
+
+  /* Render Functions */
   var renderCardTitleEdit = function () {
     $('.card-title-detail').addClass('hidden');
     $('.card-title-input').removeClass('hidden');
     //mover focus to textarea
   };
 
-  var resetAllRender = function () {
+  var renderCardDescriptionEdit = function () {
+    $('.card-description-detail').addClass('hidden');
+    $('.card-description-input').removeClass('hidden');
+  };
+
+  CV.resetAllRender = function () {
     var $inputs = $('.card-page .input');
 
     $inputs.each(function (index, el) {
       var $el = $(el);
       if (!$el.hasClass('hidden')) {
-        // $form = $el.parent().parent();
-
         $el.parent().children().not('.input').removeClass('hidden');
         $el.addClass('hidden');
-
-        // $form.submit();
       }
     });
   };
 
+  /* Event Functions */
   $('.card-title h3').on('click', function(event) {
     renderCardTitleEdit();
+  });
+
+  $('.card-edit-button').on('click', function(event) {
+    event.preventDefault();
+  });
+
+  $('.card-description-detail').on('click', function(event) {
+    console.log('click');
+    renderCardDescriptionEdit();
   });
 
   $('body').on('click', function(event) {
@@ -71,7 +84,7 @@ $(document).ready(function () {
       !($target.parent().hasClass('detail')) &&
       !($target.parent().hasClass('input')
     )) {
-      resetAllRender();
+      CV.resetAllRender();
     }
   });
 });
---
+++ app/assets/javascripts/card_view_submits.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  // console.log('click');
+
+  $('#card-title-button').on('click', function(event) {
+    event.preventDefault();
+    $("#card-title-form").submit();
+  });
+
+  $("#card-title-form").on("ajax:success", function(event, data){
+    var $form = $(this);
+    console.log(data.title);
+    $('.card-title h3').html(data.title);
+    CV.resetAllRender();
+  });
+
+  $('#card-description-button').on('click', function(event) {
+    event.preventDefault();
+    $('#card-description-form').submit();
+  });
+
+  $("#card-description-form").on("ajax:success", function(event, data){
+    var $form = $(this);
+    console.log(data.description);
+    $('.card-description h4').html(data.description);
+    CV.resetAllRender();
+  });
+});
+
