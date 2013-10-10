27bc98c303904f733ba51da3862d09893b632b2f
Sterling Forest <sterling.kxz@gmail.com>
2013-10-09 21:24:33 -0400

    removes large commented transaction and code in CardsController

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -26,16 +26,6 @@ class CardsController < ApplicationController
     end
   end
 
-  # def show
-  #   if @card = Card.includes(:checklist_items, :board).find_by_id(params[:id])
-  #     @board = @card.board
-  #     render :show
-  #   else
-  #     flash[:errors] = "Card not found"
-  #     redirect_to user_url(current_user)
-  #   end
-  # end
-
   def edit
     if @card = Card.includes({checklists: [:checklist_items]}, :participants, catagory: :board).find(params[:id])
       render :edit
@@ -50,68 +40,6 @@ class CardsController < ApplicationController
     else
       render :edit
     end
-    # begin
-#       ActiveRecord::Base.transaction do
-#         params[:checklist_items].reject! { |v| v.all? { |_, v2| v2 == "" } }
-#         p params[:card]
-#         @card.update_attributes(params[:card])
-#
-#         @checklist_items = params[:checklist_items].map do |item_params|
-#
-#           if (item_params[:id])
-#             p item_params
-#
-#             @item = ChecklistItem.find_by_id(item_params[:id])
-#             p 'got here'
-#             item_params[:id] = ""
-#             if item_params.all? { |_, v2| v2 == "" }
-#               item.destroy
-#               next nil
-#             end
-#
-#             @item.update_attributes(item_params)
-#             @item
-#           else
-#             ChecklistItem.new(item_params)
-#           end
-#         end.compact
-#
-#         @checklist_items.each do |item|
-#           unless item.id
-#             item.card_id = @card.id
-#             item.save
-#           end
-#         end
-#
-#         (params[:participants] || []).each do |p_params|
-#           p p_params[:user_id].to_i
-#           unless @card.participations.any? { |p| p.user_id == p_params[:user_id].to_i }
-#             @card.participations.build(p_params)
-#           end
-#         end
-#         p @card.participations
-#         @card.save
-#         # @card_participations = @card.participations.map do |participation|
-#         #   unless param[:participants].any? { |p| p[:user_id] == participation.id }
-#         #     participation.destroy
-#         #     next nil
-#         #   else
-#         #     next participation
-#         #   end
-#         # end.compact
-#
-#         raise "invalid" unless @checklist_items.length > 0
-#         raise "invalid" unless @card.valid? && @checklist_items.all? { |item| item.valid? }
-#         raise "invalid" unless @card.participations.all? { |p| p.valid? }
-#       end
-#
-#     rescue
-#       flash[:errors]  = @card.errors.full_messages
-#       flash[:errors] += @checklist_items.map(&:errors).map(&:full_messages).flatten
-#       render :edit
-#     else
-#       redirect_to board_url(@board)
-#     end
   end
 
   def destroy
