8ae6e42696e1663544f1fed6da47ab7dfe4f6ee2
K5nobi <sterling.kxz@gmail.com>
2013-10-14 13:36:34 -0400

    enables board scrolling to respond to new list creations

--- app/assets/javascripts/board_view/add_list.js
+++ app/assets/javascripts/board_view/add_list.js
@@ -32,6 +32,10 @@ $(document).ready(function () {
 		$('#board-lists').children().last().before(
       '<li class="list"><h4>' + data.title + '</h4><ul class="group"></ul><div class="new-card-form"><div class="new-card hidden"><form class="actually-card-new-form" action="/catagories/' + data.id + '/cards" method="POST" accept-charset="utf-8"	data-remote="true"><input type="hidden"	name="authenticity_token"	value="<%= form_authenticity_token %>"><textarea name="card[title]"></textarea><br><button class="save">Add</button></form></div><div class="add-card">Add a card...</div></div></li>'
 		);
+		
+		var minWidth = parseInt($('.board').css('min-width').slice(0, -2));
+		minWidth += 290;
+		$('.board').css('min-width', minWidth);
 	};
 	
 	$('#add-list-form').on('click', 'button', function(event) {
