5be7f2f90f0fc28b9785d93b2c0befc56a7ef82b
K5nobi <sterling.kxz@gmail.com>
2013-10-14 22:36:10 -0400

    much css, specifically 'Create Board' view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree ../../../vendor/assets/javascripts
 //= require_tree .
 
 window.CV = {};
---
+++ app/assets/javascripts/sortable/sorting.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+	// $('#board-lists').sortable();
+});
