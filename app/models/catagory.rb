201a31abd4de30a7f489e8803cf23382f7afcf80
K5nobi <sterling.kxz@gmail.com>
2013-10-16 17:47:14 -0400

    completes lists custom reorder function, complete with random gen named functions

--- app/models/catagory.rb
+++ app/models/catagory.rb
@@ -13,4 +13,40 @@ class Catagory < ActiveRecord::Base
 
   validates :board_id, :title, :sort_idx, presence: true
   validates :title, uniqueness: { scope: :board_id }
+  
+  def self.generate_reorder(catagory_set, user_id)
+    function_key = (user_id.to_s) + "_" + (SecureRandom::hex 8)
+    
+    cat_idx = {}
+    catagory_set.values.each do |catagory_params|
+      cat_idx[catagory_params[:id].to_i] = catagory_params[:sort_idx].to_i
+    end
+    
+    variables = []
+    case_statement = []
+    
+    cat_idx.keys.each_with_index do |id, idx|
+      if ( idx < cat_idx.keys.count - 1)
+        case_statement << "when id = #{id} then #{cat_idx[id]}"
+      else
+        case_statement << "else #{cat_idx[id]}"
+      end
+      variables << "var#{idx}"
+    end
+    
+    <<-SQL
+      CREATE FUNCTION reorder_#{function_key}( #{ variables.map { |var| var + " integer"}.join(', ') } )
+      RETURNS void AS
+      $$
+      UPDATE catagories
+      SET sort_idx = (case #{case_statement.join(' ')} end)
+      WHERE id IN ( #{ variables.join(', ') } );
+      $$ 
+      LANGUAGE SQL;
+      
+      SELECT reorder_#{function_key}(#{cat_idx.keys.map { |id| id.to_s }.join(', ') });
+      
+      DROP FUNCTION reorder_#{function_key}(#{variables.map { |var| "integer" }.join(', ') });
+    SQL
+  end
 end
