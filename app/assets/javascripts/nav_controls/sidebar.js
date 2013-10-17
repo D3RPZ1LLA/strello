0c1dcfab279f3720898f33d6fe77332c5af13e46
K5nobi <sterling.kxz@gmail.com>
2013-10-17 18:32:23 -0400

    enables sidebar render and derender

--- app/assets/javascripts/nav_controls/sidebar.js
+++ app/assets/javascripts/nav_controls/sidebar.js
@@ -1,1 +1,9 @@
-$
+$(document).ready(function() {
+	$('#board-sidebar-link').on('click', function(event) {
+		$('#board-sidebar').removeClass('hidden');
+	});
+	
+	$('#board-sidebar-exit').on('click', function(event) {
+		$('#board-sidebar').addClass('hidden');
+	});
+});
