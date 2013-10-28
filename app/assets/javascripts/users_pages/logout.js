88900c7be0a219ab0eb51b257f90f36826dc09e3
K5nobi <sterling.kxz@gmail.com>
2013-10-28 00:17:23 -0400

    adds profile_nav and logout menu

---
+++ app/assets/javascripts/users_pages/logout.js
@@ -0,0 +1,15 @@
+// $(document).ready(function () {
+// 	$('#logout').on('click', function () {
+// 		$.ajax({
+// 			url: '/session',
+// 			type: 'DELETE',
+// 			success: function () {
+// 				window.location.href="/";
+// 			},
+// 			error: function () {
+// 				console.log('unable to log out');
+// 			}
+// 		});
+// 	});
+// });
+// 	
