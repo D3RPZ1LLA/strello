c64f2e98c78d5472176872eb2bd2c6503f4d4e29
K5nobi <sterling.kxz@gmail.com>
2013-10-30 15:44:50 -0400

    refactors list SQL to be more readable

--- app/models/catagory.rb
+++ app/models/catagory.rb
@@ -14,22 +14,22 @@ class Catagory < ActiveRecord::Base
   validates :board_id, :title, :sort_idx, presence: true
   validates :title, uniqueness: { scope: :board_id }
   
-  def self.generate_reorder(catagory_set, user_id)
+  def self.generate_reorder(list_set, user_id)
     function_key = (user_id.to_s) + "_" + (SecureRandom::hex 8)
     
-    cat_idx = {}
-    catagory_set.values.each do |catagory_params|
-      cat_idx[catagory_params[:id].to_i] = catagory_params[:sort_idx].to_i
+    list_idx = {}
+    list_set.values.each do |list_params|
+      list_idx[list_params[:id].to_i] = list_params[:sort_idx].to_i
     end
     
     variables = []
     case_statement = []
     
-    cat_idx.keys.each_with_index do |id, idx|
-      if ( idx < cat_idx.keys.count - 1 || cat_idx.keys.count == 1)
-        case_statement << "when id = #{id} then #{cat_idx[id]}"
+    list_idx.keys.each_with_index do |id, idx|
+      if ( idx < list_idx.keys.count - 1 || list_idx.keys.count == 1)
+        case_statement << "when id = #{id} then #{list_idx[id]}"
       else
-        case_statement << "else #{cat_idx[id]}"
+        case_statement << "else #{list_idx[id]}"
       end
       variables << "var#{idx}"
     end
@@ -44,7 +44,7 @@ class Catagory < ActiveRecord::Base
       $$ 
       LANGUAGE SQL;
       
-      SELECT reorder_#{function_key}(#{cat_idx.keys.map { |id| id.to_s }.join(', ') });
+      SELECT reorder_#{function_key}(#{list_idx.keys.map { |id| id.to_s }.join(', ') });
       
       DROP FUNCTION reorder_#{function_key}(#{variables.map { |var| "integer" }.join(', ') });
     SQL
