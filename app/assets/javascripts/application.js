cde3433343dd89d5c028939bcad5964c11f6ac0f
K5nobi <sterling.kxz@gmail.com>
2013-11-06 14:28:27 -0500

    asset changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,9 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-
 //= require jquery
 //= require jquery_ujs
 //= require underscore
 //= require_tree ../../../vendor/assets/javascripts
 //= require_tree ../templates
 //= require_tree .
-
-//
