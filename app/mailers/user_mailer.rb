79986f4c0011b29d86575f247130f4f18e619192
K5nobi <sterling.kxz@gmail.com>
2013-12-06 15:12:15 -0800

    adds welcome email views

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,3 +1,13 @@
 class UserMailer < ActionMailer::Base
   default from: "from@example.com"
+  
+  def welcome_email(user)
+    @user = user
+    @url = 'http://example.com/login'
+    mail(to: user.email, subject: 'Welcome to Strello')
+  end
+  
+  def forgot_password(user)
+    # ...
+  end
 end
